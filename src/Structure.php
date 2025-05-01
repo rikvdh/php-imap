5119f7b3980a3b7613bdcf1039d2158d7e761f8e
inFreeRED <inFreeRED@whocares.fuk>
2025-05-01 21:43:47 +0200

    Attachment parts with subtype should be considered as standalone attchments

--- src/Structure.php
+++ src/Structure.php
@@ -118,7 +118,7 @@ class Structure {
             // we return these parts if there are multiple or if configured to allow a single part here (default)
             // isolated cases a single part here causes an empty body, to workaround this set
             // allow_single_parts to false in your config
-            if ( count($parts) > 1 || $config->get("options")['allow_single_parts'] ) {
+            if (count($parts) > 1 || !empty($parts[0]->subtype) || $config->get("options")['allow_single_parts']) {
                 return $parts;
             }
         }
